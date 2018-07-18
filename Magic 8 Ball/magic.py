0d8529434b77c42b09a63ff6621cee605ade20cb
Lawrence Burry <marinme@gmail.com>
2018-07-17 21:00:20 -0600

    finished the main function, removed menu with explanatory comments in code. Tests passing.

--- Magic 8 Ball/magic.py
+++ Magic 8 Ball/magic.py
@@ -1,8 +1,21 @@
-def magic():
-    pass
+import random
+import math
 
-def _menu():
-    pass
+# removed the menu stuff because I want to just work these exercises and get to my own projects for now.
+# this is more of my own learning practice and I'll be adding in some more complex stuff like unit testing
+# print statements (yes, I did look up redirecting sys.stdout and using doctest), but then I'll be working that
+# when I get in to building out network stacks from RFCs since I feel comfortable in that domain.
+
+answers = ["It is decidely so.", "Yes.", "The answer is certain.",
+           "Maybe", "It is uncertain, try again later.", "The future is cloudy",
+           "No", "Definitely not."]
+
+def magic(question):
+    if len(question) <= 0:
+        raise ValueError("Invalid question, try again.")
+    if not question[-1] == '?':
+        raise ValueError("That is not a question")
+    return answers[math.floor((random.random() * len(answers)))]
 
 if __name__=="__main__":
-    _menu()
+    print(magic("Hello?"))
