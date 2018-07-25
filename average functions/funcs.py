14e834782532714953c0a3f850fc5d8c29c2f8d3
Lawrence Burry <marinme@gmail.com>
2018-07-25 15:43:22 -0600

    Wrote out test cases, mean() is passing tests, but could stand to be driven by a non-manual process for the user input part.

--- average functions/funcs.py
+++ average functions/funcs.py
@@ -1,5 +1,8 @@
-def mean():
-    pass
+def mean(*args):
+    result = sum(args) / len(args)
+    if not result == int(result):
+        result = round(result, int(input("How many significant digits would you like? ")))
+    return result
 
 def median():
     pass
--- average functions/test_funcs.py
+++ average functions/test_funcs.py
@@ -1,10 +1,33 @@
 import funcs
 import unittest
 
+
 class TestFuncs(unittest.TestCase):
     def test_mean(self):
-        pass
+        self.assertEqual(funcs.mean(3, 4, 5), 4)
+        self.assertEqual(funcs.mean(1,2,3,4,5), 3)
+        self.assertEqual(funcs.mean(1), 1)
+        with self.assertRaises(ZeroDivisionError):
+            funcs.mean()
+        self.assertEqual(funcs.mean(1000000000, 1000000002, 1000000001), 1000000001)
+        self.assertEqual(funcs.mean(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1), 1)
+
+    def test_mean_subgoal(self):
+        self.assertEqual(funcs.mean(1,2), 1.5)  # subgoals, manually specify 1 significant digits
+        self.assertEqual(funcs.mean(1, 2, 4), 2.33)  # subgoals, manually specify 2 for significant digits
+        self.assertEqual(funcs.mean(5, 8, 10), 7.67) #subgoals, manually specify 2 for significant digits
+
     def test_median(self):
-        pass
+        self.assertEqual(funcs.median(3,4,5,7,9), list(5))
+        self.assertEqual(funcs.median(1), list(1))
+        self.assertEqual(funcs.median(1, 1, 1, 1, 1, 1), list(1))
+
+    def test_median_subgoal(self):
+        self.assertEqual(funcs.median(1, 2, 3, 4, 5, 6), list(3, 4))
+
     def test_mode(self):
-        pass
+        self.assertEqual(funcs.mode(1,1,2,3,4,5), list(1))
+        self.assertEqual(funcs.mode(1,1,2,2,2,3,4), list(3))
+
+    def test_mode_subgoal(self):
+        self.assertEqual(funcs.mode(1,1,2,2), list(1, 2))
