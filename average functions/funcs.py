7668903c8b4956a31346ee1b8adbf23b9e1e6229
Lawrence Burry <marinme@gmail.com>
2018-07-25 20:59:32 -0600

    added median() functionality. Learned that *args passes in as a tuple instead of a list. Also reworked unit tests on mean() to make use of mocks/patching for builtins.input to automate the testing instead of having to manually enter values.

--- average functions/funcs.py
+++ average functions/funcs.py
@@ -4,8 +4,13 @@ def mean(*args):
         result = round(result, int(input("How many significant digits would you like? ")))
     return result
 
-def median():
-    pass
+def median(*args):
+    vars = list(args)
+
+    if (len(vars)) % 2  == 0:
+        return list((vars[len(vars) // 2 - 1], vars[(len(vars) // 2)]))
+    else:
+        return [vars[(len(vars) // 2)]]
 
 def mode():
     pass
--- average functions/test_funcs.py
+++ average functions/test_funcs.py
@@ -1,5 +1,6 @@
 import funcs
 import unittest
+import unittest.mock
 
 
 class TestFuncs(unittest.TestCase):
@@ -13,21 +14,27 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.mean(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1), 1)
 
     def test_mean_subgoal(self):
-        self.assertEqual(funcs.mean(1,2), 1.5)  # subgoals, manually specify 1 significant digits
-        self.assertEqual(funcs.mean(1, 2, 4), 2.33)  # subgoals, manually specify 2 for significant digits
-        self.assertEqual(funcs.mean(5, 8, 10), 7.67) #subgoals, manually specify 2 for significant digits
+        with unittest.mock.patch('builtins.input') as mock:
+            instance = mock.return_value
+            instance.method.return_value = '1'
+            self.assertEqual(funcs.mean(1,2), 1.5)  # subgoals, manually specify 1 significant digits
+        with unittest.mock.patch('builtins.input') as b:
+            b.return_value = '2'
+            self.assertEqual(funcs.mean(1, 2, 4), 2.33)  # subgoals, manually specify 2 for significant digits
+            self.assertEqual(funcs.mean(5, 8, 10), 7.67) #subgoals, manually specify 2 for significant digits
 
     def test_median(self):
-        self.assertEqual(funcs.median(3,4,5,7,9), list(5))
-        self.assertEqual(funcs.median(1), list(1))
-        self.assertEqual(funcs.median(1, 1, 1, 1, 1, 1), list(1))
+        self.assertEqual(funcs.median(3, 4, 5, 7, 9), [5], "Median(3,4,5,7,9) should equal [5]")
+        self.assertEqual(funcs.median(1), [1], "Median(1) should equal [1]")
+        self.assertEqual(funcs.median(1, 1, 1, 1, 1, 1), [1, 1], "Median(1,1,1,1,1,1) should return [1,1]")
+        self.assertEqual(funcs.median(1, 2, 2, 3, 4, 4), [2, 3], "Median(1,2,2,3,4,4) should return [2,3]")
 
     def test_median_subgoal(self):
-        self.assertEqual(funcs.median(1, 2, 3, 4, 5, 6), list(3, 4))
+        self.assertEqual(funcs.median(1, 2, 3, 4, 5, 6), [3, 4])
 
     def test_mode(self):
-        self.assertEqual(funcs.mode(1,1,2,3,4,5), list(1))
-        self.assertEqual(funcs.mode(1,1,2,2,2,3,4), list(3))
+        self.assertEqual(funcs.mode(1, 1, 2, 3, 4, 5), [1,2])
+        self.assertEqual(funcs.mode(1, 1, 2, 2, 2, 3, 4), [3])
 
     def test_mode_subgoal(self):
         self.assertEqual(funcs.mode(1,1,2,2), list(1, 2))
